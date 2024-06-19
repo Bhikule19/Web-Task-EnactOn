58909dbc308eabdd804dd70ca159ee0af3f91605
Abhishek <abhishekbhikule76@gmail.com>
2024-06-19 22:09:42 +0530

    init: set up

---
+++ src/components/SwiperComponent.jsx
@@ -0,0 +1,32 @@
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const SwiperComponent = () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      className="h-screen"
+    >
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+      <SwiperSlide></SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default SwiperComponent;
